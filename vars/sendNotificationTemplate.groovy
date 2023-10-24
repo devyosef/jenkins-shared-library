184b6f44adf1fcef14a4a18de4e1973a24d17bb8
Youssef Fayoumi <98983863+devyosef@users.noreply.github.com>
2023-10-24 22:51:16 +0100

    Create sendNotificationTemplate.groovy

---
+++ vars/sendNotificationTemplate.groovy
@@ -0,0 +1,22 @@
+def renderTemplate(tempName, inputs) {
+  def engine = new groovy.text.GStringTemplateEngine()
+  def template = engine.createTemplate(tempName).make(inputs)
+  return template.toString()
+}
+
+def call(Map config = [:]) {
+  def tempName = libraryResource 'templates/successEmail.html'
+  def inputs = [
+    applicationName: env.JOB_NAME,
+    buildNumber    : env.BUILD_NUMBER,
+    buildUrl       : env.BUILD_URL,
+    message        : config.message
+  ]
+  def emailBody = renderTemplate(tempName,inputs)
+
+  // send to email
+  emailext (
+    subject: "${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+    body: emailBody,
+  )
+}
